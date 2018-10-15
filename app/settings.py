860942fbe378d55a8d8f669af138cbfdba331b98
Adam Christie <fractos@gmail.com>
2018-10-15 12:12:55 +0100

    fix loading of correct value for env var.

--- app/settings.py
+++ app/settings.py
@@ -1,16 +1,17 @@
 import os
 import database
 import re
+import distutils.util
 from database.sqlite_database import SqliteDatabase
 from database.postgresql_database import PostgreSqlDatabase
 
-DEBUG = bool(os.getenv("DEBUG", "False"))
+DEBUG = bool(distutils.util.strtobool(os.getenv("DEBUG", "False")))
 SLEEP_SECONDS = int(os.getenv("SLEEP_SECONDS"))
 ENDPOINT_DEFINITIONS_FILE = os.getenv("ENDPOINT_DEFINITIONS_FILE")
 ALERT_DEFINITIONS_FILE = os.getenv("ALERT_DEFINITIONS_FILE")
 CONNECTION_TIMEOUT = int(os.getenv("CONNECTION_TIMEOUT_SECONDS"))
 DB_TYPE = os.getenv("DB_TYPE")
-SUMMARY_ENABLED = bool(os.getenv("SUMMARY_ENABLED"))
+SUMMARY_ENABLED = bool(distutils.util.strtobool(os.getenv("SUMMARY_ENABLED")))
 SUMMARY_SLEEP_SECONDS = int(os.getenv("SUMMARY_SLEEP_SECONDS"))
 
 
